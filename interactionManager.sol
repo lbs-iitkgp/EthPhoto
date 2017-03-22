89d45f394104673652927dd3e533166c61c86a7a
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-03-22 13:39:48 +0530

    contract for managing the interactions between user contract and tag contract.

---
+++ interactionManager.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.10;
+
+contract manageInteractions {
+	mapping (string => address) _tagContractAddress;
+
+	function addPhoto(){
+		//TODO
+	}
+
+	function deletePhoto(){
+		//TODO
+	}
+
+	function viewPhoto(){
+		//TODO
+	}
+
+	function getTagContractCode(){
+		//TODO
+	}
+
+	function deployNewTagContract(){
+		//TODO
+	}
+}
