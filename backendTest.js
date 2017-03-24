cbe17eb480c2cce718436d9354d3797cfcbda562
Sandeep Kumar Pani <sandeepkumarpani888@gmail.com>
2017-03-24 16:01:53 +0530

    Added the address and abi of interactionManager

--- backendTest.js
+++ backendTest.js
@@ -19,12 +19,12 @@ else{
 	console.log("Ethereum - connected to RPC server");
 }
 
-var _interactionManagerAddress="";
-var _interactionManagerCode="";
-var _userManagerAddress="";
-var _userManagerCode="";
-var _interactonManagerContract=web3.eth.contract(interactionManagerCode).at(interactionManagerAddress);
-var _userManagerContract=web3.eth.contract(userManagerContract).at(userManagerAddress);
+var _interactionManagerAddress="0xa2c1450d9a3015094ac46ea73faca5157f3d7ed7";
+var _interactionManagerCode=[{"constant":false,"inputs":[{"name":"tag","type":"string"}],"name":"checkIFTagExists","outputs":[{"name":"","type":"bool"}],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"hash","type":"string"},{"name":"tag","type":"string"}],"name":"viewPhoto","outputs":[{"name":"","type":"bool"}],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"tag","type":"string"},{"name":"hash","type":"string"},{"name":"thumbNailHash","type":"string"}],"name":"deletePhoto","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"tag","type":"string"},{"name":"hash","type":"string"},{"name":"thumbNailHash","type":"string"}],"name":"addPhoto","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"tag","type":"string"},{"name":"contractAddress","type":"address"}],"name":"addTagContract","outputs":[],"payable":true,"type":"function"}];
+// var _userManagerAddress="";
+// var _userManagerCode="";
+var _interactonManagerContract=web3.eth.contract(_interactionManagerCode).at(_interactionManagerAddress);
+// var _userManagerContract=web3.eth.contract(userManagerContract).at(userManagerAddress);
 var _addressList=[]
 
 function init(){
